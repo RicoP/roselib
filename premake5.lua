82eb57cf9e3d01669c9df8ec392104392669ecc9
RicoP <RicoP@users.noreply.github.com>
2020-03-18 23:47:17 +0100

    base project

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+local is_visual_studio = _ACTION:find("^vs") ~= nil 
+
+workspace "ROS"
+  characterset ("MBCS")
+  configurations { "Debug", "Release" }
+  startproject "test"
+  location ".build/projects"
+  targetdir ".build/bin/%{cfg.buildcfg}"
+  debugdir "bin"
+  kind "StaticLib"
+  language "C++"
+  warnings "Off"
+  architecture "x64"
+  cppdialect "C++17"
+
+  if is_visual_studio then
+    linkoptions {
+      "/ignore:4006", -- F already defined in X.lib; second definition ignored
+    }
+  end
+
+  filter "configurations:Debug"
+    defines { "DEBUG", "EA_DEBUG" }
+    symbols "Full"
+    optimize "Off"
+    targetsuffix "-d"
+
+  filter "configurations:Release"
+    defines { "RELEASE", "NDEBUG" }
+    symbols "Off"
+    optimize "Size"
+
+project "_root"
+  kind "None"
+  files { "**" }
+  removefiles { ".git/**" }
+  removefiles { ".build/**" }
+  removefiles { "**.cpp", "**.h", "**.c" }
+
+project "ros"
+  files { "include/**.h" }
+
+project "test"
+  kind "ConsoleApp"
+  language "C++"
+  warnings "Extra"
+  includedirs { "include" }
+  files { "test/**.h", "test/**.c", "test/**.cpp" }
---
+++ test/range.cpp
@@ -0,0 +1,7 @@
+#include <ros/range.h>
+#include <iostream>
+
+int main(int argc, char** argv) {
+  std::cout << "Hello \n";
+  return 0;
+}
