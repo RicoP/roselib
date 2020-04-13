30a1472a7837053fa30cb0684cbdbdb420630399
Rico <RicoP@users.noreply.github.com>
2020-04-13 14:55:59 +0200

    add noexcept and add validity check when dereferencing invalid pointer

--- include/ros/refptr.h
+++ include/ros/refptr.h
@@ -1,34 +1,45 @@
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
 #include <new>
 
 namespace ros {
+constexpr std::uint64_t i_am_ok = 30240007572758601UL;
 
 template <class T>
 class refptr {
   struct Reference {
     size_t count;
-    int byte0;  // beginning of object container
+    std::uint64_t check;  // checksum to find bad pointer
+    int byte0;            // beginning of object container
 
-    T* ptr_unchecked() { return reinterpret_cast<T*>(&byte0); }
-    const T* ptr_unchecked() const { return reinterpret_cast<const T*>(&byte0); }
+    T* ptr_unchecked() noexcept { return reinterpret_cast<T*>(&byte0); }
+    const T* ptr_unchecked() const noexcept { return reinterpret_cast<const T*>(&byte0); }
 
-    T* ptr() {
+    T* ptr() noexcept {
       assert(count);
+      assert(check == i_am_ok);
       return ptr_unchecked();
     }
 
-    const T* ptr() const {
+    const T* ptr() const noexcept {
       assert(count);
+      assert(check == i_am_ok);
       return ptr_unchecked();
+    }
+
+    size_t use_count() const noexcept {
+      assert(check == i_am_ok);
+      return count;
     }
   };
 
   Reference* reference;
 
-  void increment() {
-    assert(reference && reference->count);
+  void increment() noexcept {
+    assert(reference);
+    assert(reference->count);
     ++reference->count;
   }
 
@@ -37,6 +48,7 @@ class refptr {
       assert(reference->count);
       --reference->count;
       if (reference->count == 0) {
+        reference->check = 0;
         // call destructor of T
         reference->ptr_unchecked()->~T();
         std::free(reference);
@@ -47,15 +59,16 @@ class refptr {
 
   template <class... Ts>
   static Reference* make_reference(Ts... args) {
-    enum { CONTAINER_SIZE = sizeof(size_t) + sizeof(T) };
+    enum { CONTAINER_SIZE = sizeof(size_t) + sizeof(uint64_t) + sizeof(T) };
     Reference* reference = reinterpret_cast<Reference*>(std::malloc(CONTAINER_SIZE));
     reference->count = 1;
+    reference->check = i_am_ok;
     new (&reference->byte0) T(args...);  // construct new Object at adress of container
     return reference;
   }
 
  public:
-  constexpr refptr() : reference(nullptr) {}
+  constexpr refptr() noexcept : reference(nullptr) {}
 
   // construct new object with parameter
   template <class... Ts>
@@ -64,10 +77,10 @@ class refptr {
   }
 
   // copy
-  refptr(const refptr<T>& rhs) : reference(rhs.reference) { increment(); }
+  refptr(const refptr<T>& rhs) noexcept : reference(rhs.reference) { increment(); }
 
   // move
-  refptr(refptr<T>&& rhs) : reference(rhs.reference) { rhs.reference = nullptr; }
+  refptr(refptr<T>&& rhs) noexcept : reference(rhs.reference) { rhs.reference = nullptr; }
 
   // destructor
   ~refptr() { release(); }
@@ -80,29 +93,29 @@ class refptr {
     return p;
   }
 
-  size_t use_count() const { return reference ? reference->count : 0; }
+  size_t use_count() const noexcept { return reference ? reference->use_count() : 0; }
 
   void release() {
     decrement();
     reference = nullptr;
   }
 
-  T* operator->() {
+  T* operator->() noexcept {
     assert(reference);
     return reference->ptr();
   }
 
-  T& operator*() {
+  T& operator*() noexcept {
     assert(reference);
     return *reference->ptr();
   }
 
-  const T* operator->() const {
+  const T* operator->() const noexcept {
     assert(reference);
     return reference->ptr();
   }
 
-  const T& operator*() const {
+  const T& operator*() const noexcept {
     assert(reference);
     return *reference->ptr();
   }
@@ -117,13 +130,11 @@ class refptr {
 
   refptr<T>& operator=(refptr<T>&& rhs) {
     assert(rhs.reference != nullptr);
-    decrement();
+    release();
     reference = rhs.reference;
     rhs.reference = nullptr;
     return *this;
   }
-
-
 
   const refptr<T>& operator=(const refptr<T>&) const = delete;
 
@@ -132,40 +143,42 @@ class refptr {
     Reference* reference;
 
    public:
-    refview() : reference(nullptr) {}
+    size_t use_count() const noexcept { return reference ? reference->use_count() : 0; }
 
-    refview(refptr<T>& ptr) : reference(ptr.reference) {}
-    refview(refptr<T>&& ptr) : reference(ptr.reference) {}
+    refview() noexcept : reference(nullptr) {}
 
-    refview(refview& ptr) : reference(ptr.reference) { ptr.reference = nullptr; }
-    refview(refview&& ptr) : reference(ptr.reference) { ptr.reference = nullptr; }
+    refview(refptr<T>& ptr) noexcept : reference(ptr.reference) {}
+    refview(refptr<T>&& ptr) noexcept : reference(ptr.reference) {}
 
-    refview& operator=(refptr<T>& rhs) {
+    refview(refview& rhs) noexcept : reference(rhs.reference) { rhs.reference = nullptr; }
+    refview(refview&& rhs) noexcept : reference(rhs.reference) { rhs.reference = nullptr; }
+
+    refview& operator=(refptr<T>& rhs) noexcept {
       reference = rhs.reference;
       return *this;
     }
 
-    refview& operator=(refptr<T>&& rhs) {
+    refview& operator=(refptr<T>&& rhs) noexcept {
       reference = rhs.reference;
       return *this;
     }
 
-    T* operator->() {
+    T* operator->() noexcept {
       assert(reference);
       return reference->ptr();
     }
 
-    T& operator*() {
+    T& operator*() noexcept {
       assert(reference);
       return *reference->ptr();
     }
 
-    const T* operator->() const {
+    const T* operator->() const noexcept {
       assert(reference);
       return reference->ptr();
     }
 
-    const T& operator*() const {
+    const T& operator*() const noexcept {
       assert(reference);
       return *reference->ptr();
     }
