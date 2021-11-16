d7e606c26cc47b035771986de6ca797a99ff5a60
Rico <RicoP@users.noreply.github.com>
2021-11-16 16:12:03 +0100

    add components

---
+++ include/components/computils.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <rose/container.h>
+#include <rose/hash.h>
+
+#include <components/guid.h>
+#include <components/localdate.h>
+
+#include <ctime>
+
+using hash_value = rose::hash_value;
+
+struct AssetRef {
+  hash_value id;
+};
+
+inline bool operator==(const AssetRef& lhs, const AssetRef& rhs) { return lhs.id == rhs.id; }
+inline void construct_defaults(AssetRef& o) { o.id = 0; }
+
+struct SoundAssetRef : public AssetRef {};
+struct TextureAssetRef : public AssetRef {};
+struct MeshAssetRef : public AssetRef {};
+struct ShaderAssetRef : public AssetRef {};
+
+struct ObjectID {
+  hash_value id;
+
+  static constexpr ObjectID empty() { return ObjectID{0}; }
+};
+
+inline ObjectID idgen() { return ObjectID{rose::hash_from_clock()}; }
+
+inline void construct_defaults(ObjectID& o) { o.id = 0; }
+
+inline bool operator==(const ObjectID& lhs, const ObjectID& rhs) { return lhs.id == rhs.id; }
+inline bool operator!=(const ObjectID& lhs, const ObjectID& rhs) { return lhs.id != rhs.id; }
+
+constexpr rose::hash_value field_hash(const char* name, const char* type) { return rose::hash(name) ^ rose::hash(type); }
+
+// Helper class to serialize fixed arrays
+template <size_t MAX, class T>
+struct CArray : rose::vector<MAX, T> {};
+
+template <size_t MAX, class TKey, class TValue>
+struct CHashmap : rose::Hashmap<MAX, TKey, TValue> {};
+
+// Helper class to serialize fixed strings
+template <size_t MAX>
+struct CString {
+  char s[MAX];
+
+  constexpr size_t capacity() const { return MAX; }
+};
+
+
+inline rose::ecs::GUID create_guid() {
+  static_assert(sizeof(hash_value) * 2 == sizeof(rose::ecs::GUID));
+  union {
+    hash_value h[2];
+    rose::ecs::GUID guid = {{0}};
+  } u;
+
+  std::time_t rawtime = time(nullptr);
+  hash_value h1 = rose::hash(rawtime);
+  rose::next(h1);
+  hash_value h2 = rose::hash_from_clock();
+
+  u.h[0] = h1;
+  u.h[1] = h2;
+
+  return u.guid;
+}
+
+inline rose::ecs::LocalDate create_local_date() {
+
+  std::time_t now = std::time(NULL);
+  std::tm * t = std::localtime(&now);
+  // clang-format off
+  int date = (
+    //        YYYYMMDDZZ
+    t->tm_year * 1000000 +
+    t->tm_mon  *   10000 +
+    t->tm_mday *     100 +
+                      00 // time zone is 0 for now
+    );
+  int time = (
+    //          HHMMSSMMM
+    t->tm_hour * 10000000 +
+    t->tm_min  *   100000 +
+    t->tm_sec  *     1000 +
+                      000 //Miliseconds are zero for now
+    );
+  // clang-format on
+
+  rose::ecs::LocalDate ld;
+  ld.date = date;
+  ld.time = time;
+  return ld;
+}
---
+++ include/components/guid.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <serializer/serializer.h>
+#include <components/localdate.h>
+#include <components/computils.h>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+namespace rose {
+namespace ecs {
+  struct GUID {
+    unsigned char data[16];
+
+    bool equals(const GUID &rhs) const { return std::memcmp(data, rhs.data, 16) == 0; }
+  };
+
+  inline void construct_defaults(GUID &o) { std::memset(o.data, 0, 16); }
+
+  inline bool operator==(const GUID &lhs, const GUID &rhs) { return lhs.equals(rhs); }
+
+  inline bool operator!=(const GUID &lhs, const GUID &rhs) { return !lhs.equals(rhs); }
+
+  inline void serialize(GUID &o, ISerializer &s) {
+    rose::string<64> guid;
+    unsigned char *p = o.data;
+    // clang-format off
+    std::sprintf(guid.data,
+                 "%02X" "%02X" "%02X""%02X-"
+                 "%02X" "%02X-"
+                 "%02X" "%02X-"
+                 "%02X" "%02X-"
+                 "%02X" "%02X" "%02X" "%02X" "%02X" "%02X",
+                 p[ 0], p[ 1], p[ 2], p[ 3],
+                 p[ 4], p[ 5],
+                 p[ 6], p[ 7],
+                 p[ 8], p[ 9],
+                 p[10], p[11], p[12], p[13], p[14], p[15]);
+    // clang-format on
+    serialize(guid, s, 36);
+  }
+
+  inline void deserialize(GUID &o, IDeserializer &s) {
+    rose::string<64> guid;
+    deserialize(guid, s);
+    char *src = guid.data;
+    unsigned char *dest = o.data;
+
+    char hex[3] = {0, 0, 0};
+    char *hexp = hex;
+
+    for (;; ++src) {
+      if (*src == '-') continue;
+      if (*src == 0) break;
+
+      *hexp = *src;
+      hexp++;
+      if (hexp == hex + 2) {
+        hexp = hex;
+        long value = strtol(hex, nullptr, 16);
+        assert(dest != o.data + 16);
+        *dest = (unsigned char)value;
+        ++dest;
+      }
+    }
+
+    assert(dest == o.data + 16);
+  }
+
+  inline void randomize(GUID &o, rose::hash_value &h) {
+    static_assert(sizeof(hash_value) * 2 == sizeof(ecs::GUID));
+    union {
+      hash_value h[2];
+      ecs::GUID guid = {{0}};
+    } u;
+    next(h);
+    u.h[0] = h;
+    next(h);
+    u.h[1] = h;
+    o = u.guid;
+  }
+}  // namespace ecs
+
+inline hash_value hash(ecs::GUID &o) {
+  static_assert(sizeof(hash_value) * 2 == sizeof(ecs::GUID));
+  union {
+    hash_value h[2];
+    ecs::GUID guid;
+  } u;
+  u.guid = o;
+  return u.h[0] ^ u.h[1];
+}
+}  // namespace rose
