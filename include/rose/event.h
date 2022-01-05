decd0f17e00eb046d164fef0a00a39d3915dedd2
Rico P <RicoP@users.noreply.github.com>
2022-01-05 22:58:58 +0100

    Update event.h

--- include/rose/event.h
+++ include/rose/event.h
@@ -7,14 +7,22 @@ namespace rose {
 // A Pointer to the current data section representing the event.
 // Keep in mind that it is only valid for the lifetime of the Queue.
 struct Event {
-  rose::hash_value event_id;
-  size_t padded_event_size;
-  char *data;  // pointing on firs byte of event struct
+  rose::hash_value event_id = 0;
+  size_t padded_event_size = 0;
+  char *data = nullptr;  // pointing on first byte of event struct
 
   template <class T>
-  const T *try_get() const {
+  const T *get() const {
     if (rose::type_id<T>::VALUE == event_id) {
       return reinterpret_cast<const T *>(data);
+    }
+    return nullptr;
+  }
+
+  template <class T>
+  T *get_mutable() {
+    if (rose::type_id<T>::VALUE == event_id) {
+      return reinterpret_cast<T *>(data);
     }
     return nullptr;
   }
