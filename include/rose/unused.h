d18bee7ebad56c9a583d43ec98e118bb2c6adb3a
Rico P <RicoP@users.noreply.github.com>
2022-10-01 15:50:49 +0200

    Update unused.h

--- include/rose/unused.h
+++ include/rose/unused.h
@@ -1,19 +1,5 @@
 #pragma once
 
-//Basically the (void)(unused_value) pattern to keep the
-//compiler quite about unused variables. Also a good 
-//replacement for empty method macros like ASSERT in Release.
-//    #define ASSERT(COND, MSG) rose::unused(COND, MSG)
-//will guarantee the execution of the expression COND and MSG.
 namespace rose {
-template <class Thead>
-void unused(const Thead &) {
-}
-/*
-template <class Thead>
-void unused(const Thead & head, ...) {
-  rose::unused(head);
-  rose::unused(...);
-}
-*/
+void unused(...) {}
 }  // namespace rose
