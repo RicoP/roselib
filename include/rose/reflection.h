c8751511448fb371449b496ec23b4286c8090776
Rico <RicoP@users.noreply.github.com>
2023-01-14 20:44:32 +0100

    update TypeInfo signature.

--- include/rose/reflection.h
+++ include/rose/reflection.h
@@ -14,13 +14,18 @@ namespace rose::reflection {
         // memory alignment
         std::size_t memory_alignment = 16;
 
+        // Keep in mind that points are bound to the running dll.
+        // The Typeinfo object should be desolved before the DLL is unloaded.
+
+        // Name of the type. Should be unique. (In DLL memory!)
+        char * name;
+
+        // static function pointers to constructor and destructor. (In DLL memory!)
         void (*fp_default_construct)(void *) = nullptr;
         void (*fp_default_destruct)(void *) = nullptr;
 
+        // static function pointers to serialization methods. (In DLL memory!)
         void (*fp_serialize)(void *, ISerializer &) = nullptr;
         void (*fp_deserialize)(void *, IDeserializer &) = nullptr;
-
-        // Name of the type. Should be unique. (Not a char* because lifetime of string is bound to system.dll)
-        char name[64];
     };
 }
