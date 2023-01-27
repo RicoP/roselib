5759899d9443bdc11f35092e88fc634446a2d700
Rico <RicoP@users.noreply.github.com>
2023-01-27 23:06:42 +0100

    use custom hash function because __FILE__ either uses / or \ depending on random external factors

--- include/rose/world.h
+++ include/rose/world.h
@@ -6,18 +6,32 @@
 #include <rose/reflection.h>
 
 namespace rose::internal {
+constexpr rose::hash_value RoseUniqueClassImplHash(const char* file, rose::hash_value line) {
+  rose::hash_value MagicPrime = 0x00000100000001b3ULL;
+  rose::hash_value Hash = 0xcbf29ce484222325ULL;
+
+  for (; *file; file++) {
+    char c = *file;
+    if(c == '\\') c = '/';
+    Hash = (Hash ^ c) * MagicPrime;
+  }
+
+  return Hash ^ line;
+}
+
 struct RoseUniqueClassImplBase {};
 
-template <size_t UNIQUE_ID>
+template <rose::hash_value UNIQUE_ID>
 struct RoseUniqueClassImpl : RoseUniqueClassImplBase {
-    enum { class_id = UNIQUE_ID };
+    static inline char* file = __FILE__;
+    static inline rose::hash_value type_id = UNIQUE_ID;
 };
 }
 
 // TODO: Wrap 'rose::hash(__FILE__)' in a constexpr function that only takes
 //       the part after '/include/' or '/source/' so the same source file
 //       generates the same ID on different machines.
-#define RoseUniqueClass rose::internal::RoseUniqueClassImpl<rose::hash(__FILE__) ^ __LINE__>
+#define RoseUniqueClass rose::internal::RoseUniqueClassImpl<rose::internal::RoseUniqueClassImplHash(__FILE__, __LINE__)>
 
 namespace rose::world {
     template<class T>
@@ -32,14 +46,12 @@ namespace rose::world {
     void register_utility(T * instance) {
         static_assert( std::is_base_of<rose::internal::RoseUniqueClassImplBase, T>::value, "T must enherit from RoseUniqueClass" );
         assert(instance);
-        rose::hash_value type_id = T::class_id;
-        c_rose_internal_register_utility_class_instance(c_rose_subsystem_instance_id, type_id, instance);
+        c_rose_internal_register_utility_class_instance(c_rose_subsystem_instance_id, T::type_id, instance);
     }
 
     template<class T>
     T & get_utility() {
-        rose::hash_value type_id = T::class_id;
-        void * ptr = c_rose_internal_get_utility_class_instance(c_rose_subsystem_instance_id, type_id);
+        void * ptr = c_rose_internal_get_utility_class_instance(c_rose_subsystem_instance_id, T::type_id);
         assert(ptr);
         return *std::launder(reinterpret_cast<T*>(ptr));
     }
