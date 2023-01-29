6411f6af00cb53732ed853325daabaa631a85e51
Rico <RicoP@users.noreply.github.com>
2023-01-30 00:52:17 +0100

    add operators for raylib primitives

--- include/rose/raylibutils/raylibutils.h
+++ include/rose/raylibutils/raylibutils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <raylib.h>
+#include <raymath.h>
 #include <rose/hash.h>
 #include <rose/singleton.h>
 #include <rose/raylibutils/assetmanager.h>
@@ -79,9 +80,40 @@ inline void deserialize(RaylibAssetModel &o, IDeserializer &s) {
 }
 }
 
-
 namespace rose {
 inline ::rose::hash_value hash(const RaylibAssetModel &o) {
 	return ::rose::hash(o.path);
 }
 }
+
+// Operators
+
+// Vector2
+inline Vector2 & operator+=(Vector2 & lhs, Vector2 rhs) { lhs.x += rhs.x; lhs.y += rhs.y; return lhs; }
+inline Vector2 & operator-=(Vector2 & lhs, Vector2 rhs) { lhs.x -= rhs.x; lhs.y -= rhs.y; return lhs; }
+inline Vector2 & operator*=(Vector2 & lhs, float rhs)   { lhs.x *= rhs;   lhs.y *= rhs;   return lhs; }
+inline Vector2 operator+(Vector2 lhs, Vector2 rhs)      { return lhs += rhs; }
+inline Vector2 operator-(Vector2 lhs, Vector2 rhs)      { return lhs -= rhs; }
+inline Vector2 operator*(Vector2 lhs, float rhs)        { return lhs *= rhs; }
+
+// Vector3
+inline Vector3 & operator+=(Vector3 & lhs, Vector3 rhs) { lhs.x += rhs.x; lhs.y += rhs.y; lhs.z += rhs.z; return lhs; }
+inline Vector3 & operator-=(Vector3 & lhs, Vector3 rhs) { lhs.x -= rhs.x; lhs.y -= rhs.y; lhs.z -= rhs.z; return lhs; }
+inline Vector3 & operator*=(Vector3 & lhs, float rhs)   { lhs.x *= rhs;   lhs.y *= rhs;   lhs.z *= rhs;   return lhs; }
+inline Vector3 operator+(Vector3 lhs, Vector3 rhs)      { return lhs += rhs; }
+inline Vector3 operator-(Vector3 lhs, Vector3 rhs)      { return lhs -= rhs; }
+inline Vector3 operator*(Vector3 lhs, float rhs)        { return lhs *= rhs; }
+
+// Vector4
+inline Vector4 & operator+=(Vector4 & lhs, Vector4 rhs) { lhs.x += rhs.x; lhs.y += rhs.y; lhs.z += rhs.z; lhs.w += rhs.w; return lhs; }
+inline Vector4 & operator-=(Vector4 & lhs, Vector4 rhs) { lhs.x -= rhs.x; lhs.y -= rhs.y; lhs.z -= rhs.z; lhs.w -= rhs.w; return lhs; }
+inline Vector4 & operator*=(Vector4 & lhs, float rhs)   { lhs.x *= rhs;   lhs.y *= rhs;   lhs.z *= rhs;   lhs.w *= rhs;   return lhs; }
+inline Vector4 operator+(Vector4 lhs, Vector4 rhs)      { return lhs += rhs; }
+inline Vector4 operator-(Vector4 lhs, Vector4 rhs)      { return lhs -= rhs; }
+inline Vector4 operator*(Vector4 lhs, float rhs)        { return lhs *= rhs; }
+
+// Matrix
+inline Matrix operator+(Matrix lhs, Matrix rhs)   { return MatrixAdd(lhs, rhs); }
+inline Matrix operator-(Matrix lhs, Matrix rhs)   { return MatrixSubtract(lhs, rhs); }
+inline Matrix operator*(Matrix lhs, Matrix rhs)   { return MatrixMultiply(lhs, rhs); }
+inline Vector3 operator*(Vector3 lhs, Matrix rhs) { return Vector3Transform(lhs, rhs); }
