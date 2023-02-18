0b66f3247369eff1ae704c0757626dec0270ae50
Rico <RicoP@users.noreply.github.com>
2023-02-18 16:42:14 +0100

    Create handle.h

---
+++ include/rose/handle.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace rose {
+    // Lokal handles are temporary ids that are only valid in the current context.
+    typedef int local_handle;
+
+    /*
+    struct GUID {
+        char data[16];
+    };
+
+    // Global handles are ids that are only valid beyond the application lifetime.
+    typedef GUID global_handle;
+    */
+}
