90d379e7939e34efdd0ee56c87195970c12eecf2
Rico <RicoP@users.noreply.github.com>
2023-03-02 00:40:45 +0100

    first draft of utf8 wrapper

--- include/rose/utf8.h
+++ include/rose/utf8.h
@@ -3,85 +3,171 @@
 #include <iterator>
 
 namespace rose {
-typedef uint32_t utf8code;
+typedef int utf8code;
 
-struct utf8range {
-	const char * head;
-	int tail_in_bytes;
+struct SimpleSpan {
+  const char * s = nullptr;
+  int size = 0;
 
-	utf8code peek(int & bytes) const;
-	utf8code peek() const {
-        int bytes = 0;
-        return peek(bytes);
-    }
-
-    bool operator==(utf8range rhs) const { return head == rhs.head && tail_in_bytes == rhs.tail_in_bytes; }
-    bool operator!=(utf8range rhs) const { !(*this == rhs); }
+  bool operator==(SimpleSpan rhs) const { return s == rhs.s && size == rhs.size; }
+  bool operator!=(SimpleSpan rhs) const { return !(*this == rhs); }
 };
 
-struct utf8iterator : utf8range {
-    // Required typedefs
-    typedef std::ptrdiff_t difference_type;
-    typedef utf8range value_type;
-    typedef utf8range* pointer;
-    typedef utf8range& reference;
-    typedef std::forward_iterator_tag iterator_category;
+//https://github.com/RandyGaul/cute_framework/blob/5f68097c836802daad208f2fcb85256ddd136745/src/cute_string.cpp#LL553C48-L553C57
+inline utf8code cf_decode_UTF8(SimpleSpan * span)
+{
+  
+  if(span->size == 0) {
+    return 0;
+  } 
 
-    void increment() {
-    	int bytes;
-    	utf8code code = peek(bytes);
-    	(void)(code);
-        //TODO: check overflow
-    	head += bytes;
-    	tail_in_bytes -= bytes;
-    }
+  SimpleSpan end;
+  end.s = span->s + span->size;
+  end.size = 0;
 
-    // Required member functions
-    utf8iterator& operator++() {
-    	increment();
-    	return *this;
-    }
+  unsigned char c = *span->s++;
+  span->size--;
 
-    utf8iterator operator++(int) {
-    	utf8iterator copy = *this;
-    	increment();    
-    	return copy;
-    }
+  int extra = 0;
+  int min = 0;
+  int codepoint = 0;
+     if (c >= 0xF0) { codepoint = c & 0x07; extra = 3; min = 0x10000; }
+  else if (c >= 0xE0) { codepoint = c & 0x0F; extra = 2; min = 0x800; }
+  else if (c >= 0xC0) { codepoint = c & 0x1F; extra = 1; min = 0x80; }
+  else if (c >= 0x80) { codepoint = 0xFFFD; }
+  else codepoint = c;
 
-    reference operator*() { return *this; }
-    pointer operator->() { return this; }
+  if(extra > span->size) {
+    *span = end;
+    return 0;
+  }
+  span->size -= extra;
+
+  while (extra--) {
+    c = *span->s++;
+    if ((c & 0xC0) != 0x80) { codepoint = 0xFFFD; break; }
+    codepoint = ((codepoint) << 6) | (c & 0x3F);
+  }
+  if (codepoint < min) codepoint = 0xFFFD;
+
+  if(codepoint == 0 || codepoint == 0xFFFD) *span = end;
+  return codepoint;
+}
+
+/*
+struct utf8iterator {
+  const char * head;
+
+  utf8iterator(const char * head_) : head(head_) {}
+
+  // Required typedefs
+  typedef std::ptrdiff_t difference_type;
+  typedef char value_type;
+  typedef const char* pointer;
+  typedef const char& reference;
+  typedef std::forward_iterator_tag iterator_category;
+
+  void increment() {
+    int bytes = 999;
+    utf8code code = cf_decode_UTF8(head, &bytes);
+    (void)(code);
+    //TODO: check overflow
+    head += bytes;
+  }
+
+  // Required member functions
+  utf8iterator& operator++() {
+    increment();
+    return *this;
+  }
+
+  utf8iterator operator++(int) {
+    utf8iterator copy = *this;
+    increment();  
+    return copy;
+  }
+
+  const reference operator*() const { return *head; }
+  const pointer operator->() const { return head; }  
+  
+  bool operator==(utf8span rhs) const { return head == rhs.head && tail_in_bytes == rhs.tail_in_bytes; }
+  bool operator!=(utf8span rhs) const { return !(*this == rhs); }
 };
+*/
 
 struct utf8span {
-	utf8iterator it;
+  SimpleSpan span;
 
-    utf8span(const char * str, int length) : it({str, length}) {}
+  utf8code peek() const {
+    SimpleSpan copy = span;
+    return cf_decode_UTF8(&copy);
+  }
 
-	utf8iterator begin() const { return it; }
-	utf8iterator end() const { return { it.head + it.tail_in_bytes, 0 }; }
+  utf8code increment() {
+    return cf_decode_UTF8(&span);
+  }
 
-    int length() const {
-        int result = 0;
-        auto my_end = end();
-        for(auto current = begin(); current != my_end; ++current) {
-            ++result;
-        }
-        return result;
+  utf8span() : span {} {}
+  utf8span(const char * str, int length) : span { str, length } {}
+  SimpleSpan begin() const { return span; }
+  SimpleSpan end() const { return SimpleSpan { span.s + span.size, 0 }; }
+
+  int length() const {
+    int result = 0;
+    auto my_end = end();
+    for(auto current = begin(); current != my_end; cf_decode_UTF8(&current)) {
+      ++result;
     }
+    return result;
+  }
+
+  bool operator==(utf8span rhs) const { return span == rhs.span; }
+  bool operator!=(utf8span rhs) const { return !(*this == rhs); }
 };
 
-struct utf8reader {
-    int handle = 0;
+//https://dev.to/totally_chase/implementing-functional-type-safe-polymorphism-in-c-10b9
+struct utf8vfactory {
+  void * context = nullptr;
+  utf8span (* const fp_get   )(void *, int) = 0;
+  int    (* const fp_copy    )(void *, int) = 0;
+  void   (* const fp_destroy )(void *, int) = 0;
+  //int  (* const fp_append  )(void *, utf8span) = 0;
 
-	virtual ~utf8reader() {}
-	virtual utf8span get(int) const = 0;
-
-	operator utf8span() const { return get(handle); }
-    utf8span operator->() const { return get(handle); }
+  utf8span get   (int handle) const { return fp_get(context, handle); }
+  int    copy  (int handle) const { return fp_copy(context, handle); }
+  void   destroy (int handle) const { fp_destroy(context, handle); }
+  //int  append  (utf8span span) { return fp_append(context, span); }
 };
 
-struct utf8writer : utf8reader {
-	virtual utf8writer append(utf8span) = 0;
+struct utf8 {
+  const utf8vfactory * factory = nullptr;
+  int handle = 0;
+
+  utf8(const utf8vfactory * factory_, int handle_) : factory(factory_), handle(handle_) {};
+  utf8(const utf8 & rhs) : factory(rhs.factory) {                       handle = factory->copy(handle); }
+  utf8(utf8 && rhs) : factory(rhs.factory), handle(rhs.handle) { rhs.handle = 0; rhs.factory = nullptr; }
+  ~utf8()          { if(factory) factory->destroy(handle);           handle = 0;     factory = nullptr; }
+
+  utf8span get() const { return factory->get(handle); }
+  operator utf8span() const { return get(); }
+  utf8span operator->() const { return get(); }
+  int length() const { return get().length(); }
+
+  /*
+  utf8 append(utf8span span) { 
+    int new_handle = factory->append(span);
+    return utf8 { factory, new_handle };
+  }
+
+  utf8 operator+= (const utf8 & rhs) {
+    return append(rhs);
+  }
+
+  utf8 operator+ (const utf8 & rhs) const { 
+    utf8 lhs = *this;
+    return lhs.append(rhs);
+  }
+  */
 };
 
 }
---
+++ test/utf8_test.cpp
@@ -0,0 +1,65 @@
+#include <rose/utf8.h>
+#include <vector>
+#include <map>
+#include <catch2/catch.hpp>
+#include <utility>
+
+static int handle_counter = 0;
+static std::vector<char> mem_pool;
+static std::map<int, rose::utf8span> ranges_pool;
+
+static rose::utf8 make_new_utf8(rose::utf8vfactory * factory, const char * from, int length_in_bytes) {
+    auto i = mem_pool.size();
+    mem_pool.resize(i + length_in_bytes);
+    char * dst = &mem_pool[i];
+    std::memcpy(dst, from, length_in_bytes);
+    rose::utf8span span(dst, length_in_bytes);
+    handle_counter++;
+    ranges_pool[handle_counter] = span;
+    return rose::utf8(factory, handle_counter);
+}
+
+template<int N>
+rose::utf8 make_new_utf8(rose::utf8vfactory * factory, const char (&from)[N]) {
+    return make_new_utf8(factory, from, N-1);
+}
+
+static rose::utf8span factory_get(void *, int handle) {
+    return ranges_pool[handle];
+}
+
+static int factory_copy(void *, int handle) {
+    handle_counter++;
+    ranges_pool[handle_counter] = ranges_pool[handle];
+    return handle_counter;
+}
+
+static void factory_destroy(void *, int handle) {
+    //leaves a hole in the pool
+    ranges_pool.erase(handle);
+}
+
+//static rose::utf8vfactory s_factory;
+static rose::utf8vfactory make_factory() {
+    return rose::utf8vfactory {
+        .fp_get     = factory_get,
+        .fp_copy    = factory_copy,
+        .fp_destroy = factory_destroy
+    };
+}
+
+TEST_CASE("UTF8 Sanity Check", "[rose string]") {
+    rose::utf8vfactory factory = make_factory();
+    rose::utf8 s1 = make_new_utf8(&factory, "Hello");
+    REQUIRE(5 == s1.length());
+
+    {
+      auto it = s1.get();
+      REQUIRE('H' == it.increment());
+      REQUIRE('e' == it.increment());
+      REQUIRE('l' == it.increment());
+      REQUIRE('l' == it.increment());
+      REQUIRE('o' == it.increment());
+      REQUIRE(0 == it.increment());
+    }
+}
