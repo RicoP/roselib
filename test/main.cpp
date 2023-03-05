4069d4a9cbd2259dd77d98b3569ec8549738b7e7
Rico <RicoP@users.noreply.github.com>
2023-03-05 03:26:40 +0100

    make compiler happy

--- test/main.cpp
+++ test/main.cpp
@@ -1,2 +1,5 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch2/catch.hpp>
+
+#include <rose/testing.h>
+rose::test::TestFactory* g_TestFactory = nullptr;
